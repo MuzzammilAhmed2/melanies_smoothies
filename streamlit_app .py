3b53c8954bd2690d24aced7f22ffa9af4dd2afd5
MuzzammilAhmed2 <155829409+MuzzammilAhmed2@users.noreply.github.com>
2026-04-08 17:19:05 +0500

    create streamlit_app .py
    copied from my custom smoothies SIS app

--- streamlit_app .py
+++ streamlit_app .py
@@ -1,13 +1,12 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app.
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 
 name_on_order = st.text_input("Name on Smoothie:")
